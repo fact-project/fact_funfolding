6e42c9b04f5c3ec949cd01e09639fe8462262617
Jens Buß <jens.buss@tu-dortmund.de>
2019-06-14 10:03:40 +0200

    allow to save additional features to json

--- fact_funfolding/io.py
+++ fact_funfolding/io.py
@@ -1,10 +1,11 @@
 import json
 import astropy.units as u
+import numpy as np
 
 from fact.analysis.statistics import POINT_SOURCE_FLUX_UNIT
 
 
-def save_spectrum(outputfile, bins, flux, flux_err, counts, counts_err, g=None, bg=None, **metadata):
+def save_spectrum(outputfile, bins, flux, flux_err, counts, counts_err, g=None, bg=None, add_features=None, **metadata):
     bins = bins.to(u.GeV)
     bin_centers = 0.5 * (bins[:-1] + bins[1:])
 
@@ -39,14 +40,32 @@ def save_spectrum(outputfile, bins, flux, flux_err, counts, counts_err, g=None, 
         **metadata
     }
 
-    if g is not None:
-        data['g'] = g.tolist()
+    add_if_not_none(data, g, 'g')
+    add_if_not_none(data, bg, 'bg')
 
-    if bg is not None:
-        data['bg'] = bg.tolist()
+    if add_features is not None:
+        for key in add_features:
+            add_if_not_none(data, add_features[key], key)
+
+    if metadata is not None:
+        for key in metadata:
+            add_if_not_none(data, metadata[key], key)
 
     with open(outputfile, 'w') as f:
         json.dump(data, f, indent=4)
+
+
+def add_if_not_none(result_dict, value, key):
+    if value is not None:
+        if isinstance(value, u.quantity.Quantity):
+            result_dict[key] = {
+                'value': value.value if value.isscalar else value.value.tolist(),
+                'unit': str(value.unit)
+            }
+        elif isinstance(value, np.ndarray):
+            result_dict[key] = value.tolist() if len(value) > 1 else value
+        else:
+            result_dict[key] = value
 
 
 def read_spectrum(inputfile):
