52bdb05ab3ae310e48b5537a7a10e2200ba89f0d
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-07-16 12:57:19 +0200

    Fix logspace for astropy 4

--- fact_funfolding/binning.py
+++ fact_funfolding/binning.py
@@ -4,8 +4,8 @@ import astropy.units as u
 
 def logspace_binning(e_low, e_high, e_ref, n_bins):
     bins = np.logspace(
-        np.log10(e_low / e_ref).to(u.dimensionless_unscaled),
-        np.log10(e_high / e_ref).to(u.dimensionless_unscaled),
+        np.log10(e_low / e_ref).to_value(u.dimensionless_unscaled),
+        np.log10(e_high / e_ref).to_value(u.dimensionless_unscaled),
         n_bins + 1,
     )
     return np.append(-np.inf, np.append(bins, np.inf)) * e_ref
